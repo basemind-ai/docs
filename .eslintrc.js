687bc4d1a709eaa92e9f00d2b161234182192b96
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-12-18 07:15:49 +0100

    feat: scaffolded the docs site

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	root: true,
+	extends: [
+		'plugin:@docusaurus/recommended',
+		//'plugin:mdx/recommended'
+	],
+	overrides: [
+		{
+			files: ['*ts', '*tsx'],
+			extends: [
+				'@tool-belt/eslint-config/react',
+				'plugin:@docusaurus/recommended',
+			],
+			parserOptions: {
+				project: './tsconfig.json',
+			},
+			rules: {
+				'import/no-unresolved': 0,
+			},
+		},
+		{
+			files: ['*.mdx'],
+			extends: [
+				'plugin:mdx/recommended',
+				'plugin:@docusaurus/recommended',
+			],
+			settings: {
+				'mdx/code-blocks': true,
+			},
+		},
+	],
+};
